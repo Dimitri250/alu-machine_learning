fe9041bccc95ea8fe20c4139c00ecdbaa71f2bd3
DimtiriKwihangana <kwihanganadimtiri@gmail.com>
2024-05-19 17:37:34 +0200

    7

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -4,6 +4,7 @@
     that concatenates two matrices along a specific axis
 '''
 
+
 def cat_matrices2D(mat1, mat2, axis=0):
     '''
         The function def cat_matrices2D(mat1, mat2, axis=0)
@@ -15,4 +16,3 @@ def cat_matrices2D(mat1, mat2, axis=0):
         return [mat1[i] + mat2[i] for i in range(len(mat1))]
     else:
         return None
-    
