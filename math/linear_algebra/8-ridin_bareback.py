ce7129a61056423c0ef2f7d6cdc86998b02b5e3a
DimtiriKwihangana <kwihanganadimtiri@gmail.com>
2024-05-19 17:27:31 +0200

    eith

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -4,7 +4,6 @@
     that performs matrix multiplication
 '''
 
-
 def mat_mul(mat1, mat2):
     '''
         The function def mat_mul(mat1, mat2)
@@ -13,11 +12,10 @@ def mat_mul(mat1, mat2):
     if len(mat1[0]) == len(mat2):
         return [
             [
-                sum([mat1[i][k] * mat2[k][j] for k in range(len(mat1[0]))])
+                sum(mat1[i][k] * mat2[k][j] for k in range(len(mat1[0])))
                 for j in range(len(mat2[0]))
             ]
             for i in range(len(mat1))
         ]
     else:
-        return None
-    
+        return None   
