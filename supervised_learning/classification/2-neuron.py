1428850587aa25958f15b3e9c37778eaf0dca94d
DimtiriKwihangana <kwihanganadimtiri@gmail.com>
2024-09-14 17:06:09 +0200

    white space

--- supervised_learning/classification/2-neuron.py
+++ supervised_learning/classification/2-neuron.py
@@ -43,10 +43,9 @@ class Neuron:
         """
         Calculates the forward propagation of the neuron.
 
-        Args:
-            X (numpy.ndarray): Input data of shape (nx, m), where
-                               nx is the number of input features
-                               and m is the number of examples.
+        Args: X (numpy.ndarray): Input data of shape (nx, m), where
+        nx is the number of input features
+        and m is the number of examples.
 
         Returns:
             numpy.ndarray: The activated output of the neuron (A).
